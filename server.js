dcfc783c8c022fe85461d03a0eaf080bb941af5b
Anne Odom <afodom207@gmail.com>
2026-01-23 08:20:41 -0600

    MODIFY: Modify PORT to accept environment variable for port, to work with render.com

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const fs = require("fs/promises");
 const notesData = require("./db/db.json");
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const DB_DIR = path.join(__dirname, "db");
 const DB_FILE = path.join(DB_DIR, "db.json");
